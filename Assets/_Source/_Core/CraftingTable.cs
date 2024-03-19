94f8b4a12238a2d7f39b77475aaf433180e8bf2c
Anton Muravev <ayumuravev@edu.hse.ru>
2024-03-19 22:23:01 +0300

    craftingTable 0.1
    add!: pop up cloud when player next to the crafting table
    add!: crafting panel on E
    add: ./idea to .gitignore

---
+++ Assets/_Source/_Core/CraftingTable.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class CraftingTable : MonoBehaviour
+{
+    [SerializeField] private GameObject messageCloud;
+    [SerializeField] private GameObject craftingPanel;
+    [SerializeField] private GameObject storagePanel;
+    [SerializeField] private GameObject overlay;
+    
+    private bool _canCraft;
+    
+    private void Update()
+    {
+        if (!_canCraft) return;
+        
+        if (Input.GetKeyDown(KeyCode.E) && !storagePanel.activeSelf)
+        {
+            craftingPanel.SetActive(!craftingPanel.activeSelf);
+            overlay.SetActive(!overlay.activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !craftingPanel.activeSelf)
+        {
+            storagePanel.SetActive(!storagePanel.activeSelf);
+            overlay.SetActive(!overlay.activeSelf);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.transform.CompareTag("Player")) return;
+
+        messageCloud.SetActive(true);
+        _canCraft = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.transform.CompareTag("Player")) return;
+
+        messageCloud.SetActive(false);
+        _canCraft = false;
+    }
+}
